075627e6e19c4f1a94f5d66b661a37e571aeaafb
Starovoytov Alexey <relz0071@gmail.com>
2018-02-01 20:07:31 +0000

    Инициализирован проект

---
+++ Starovoytov_Alexey/MonteCarloPI/Constant.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const size_t ARG_COUNT = 3;
+const size_t RADIUS = 1;
---
+++ Starovoytov_Alexey/MonteCarloPI/main.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Constant.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	size_t iterationCount = stoi(argv[1]);
+	size_t threadCount = stoi(argv[2]);
+
+	return EXIT_SUCCESS;
+}
