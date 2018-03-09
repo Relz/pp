1f95dd030f914ac922b8f2c843f58c1ddc7f4510
Starovoytov Alexey <relz0071@gmail.com>
2018-03-09 15:57:15 +0000

    Добавлено свойство задачи - isApproved

--- lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.cpp
+++ lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.cpp
@@ -1,30 +1,58 @@
 #include "Task.h"
 
-Task::Task(Programmer * m_assignee)
+Task::Task(Programmer * assignee)
+	: m_assignee(assignee)
+{}
+
+Programmer * Task::GetAssignee()
 {
-	m_assignee = m_assignee;
+	return m_assignee;
 }
 
 bool Task::IsDone() const
 {
-	return m_isDone;
+	return *m_isDone;
 }
 
 void Task::SetDone(bool isDone)
 {
-	if (m_isDone != isDone)
+	if (*m_isDone != isDone)
 	{
-		m_isDone = isDone;
+		*m_isDone = isDone;
 		m_doneChangeSignal(isDone);
 	}
 }
 
+bool Task::IsApproved() const
+{
+	return *m_isApproved;
+}
+
+void Task::SetApproved(bool isApproved)
+{
+	if (*m_isApproved != isApproved)
+	{
+		*m_isApproved = isApproved;
+		m_approvedChangeSignal(isApproved);
+	}
+}
+
 boost::signals2::connection Task::DoOnDoneChange(
-	const boost::signals2::signal<void(bool isDone)>::slot_type & slot, bool doAction = false)
+	const boost::signals2::signal<void(bool isDone)>::slot_type & slot, bool doAction)
 {
 	if (doAction)
 	{
-		slot(m_isDone);
+		slot(*m_isDone);
 	}
 	return m_doneChangeSignal.connect(slot);
+}
+
+boost::signals2::connection Task::DoOnApprovedChange(
+	boost::signals2::signal<void(bool isDone)>::slot_type const & slot, bool doAction)
+{
+	if (doAction)
+	{
+		slot(*m_isApproved);
+	}
+	return m_approvedChangeSignal.connect(slot);
 }
--- lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.h
+++ lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.h
@@ -8,13 +8,20 @@ class Task
 {
 public:
 	Task(Programmer * m_assignee);
+	Programmer * GetAssignee();
 	bool IsDone() const;
 	void SetDone(bool isDone);
+	bool IsApproved() const;
+	void SetApproved(bool isApproved);
 	boost::signals2::connection DoOnDoneChange(
-		boost::signals2::signal<void(bool isDone)>::slot_type const & slot, bool doAction);
+		boost::signals2::signal<void(bool isDone)>::slot_type const & slot, bool doAction = false);
+	boost::signals2::connection DoOnApprovedChange(
+		boost::signals2::signal<void(bool isDone)>::slot_type const & slot, bool doAction = false);
 
 private:
-	bool m_isDone = false;
+	bool * m_isDone = new bool(false);
+	bool * m_isApproved = new bool(false);
 	Programmer * m_assignee;
 	boost::signals2::signal<void(bool isDone)> m_doneChangeSignal;
+	boost::signals2::signal<void(bool isApproved)> m_approvedChangeSignal;
 };
