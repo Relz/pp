3038d9d7340f5a534a6edd6293dbc9aacfa3f017
Starovoytov Alexey <relz0071@gmail.com>
2018-03-09 07:48:58 +0000

    Усовершенствован класс задачи На изменение состояния задачи можно подписаться

--- lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.cpp
+++ lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.cpp
@@ -5,7 +5,26 @@ Task::Task(Programmer * m_assignee)
 	m_assignee = m_assignee;
 }
 
+bool Task::IsDone() const
+{
+	return m_isDone;
+}
+
 void Task::SetDone(bool isDone)
 {
-	m_done = isDone;
+	if (m_isDone != isDone)
+	{
+		m_isDone = isDone;
+		m_doneChangeSignal(isDone);
+	}
+}
+
+boost::signals2::connection Task::DoOnDoneChange(
+	const boost::signals2::signal<void(bool isDone)>::slot_type & slot, bool doAction = false)
+{
+	if (doAction)
+	{
+		slot;
+	}
+	return m_doneChangeSignal.connect(slot);
 }
--- lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.h
+++ lw2/Starovoytov_Alexey/TaskAboutProgrammers/Task.h
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <boost/signals2.hpp>
+
 class Programmer;
 
 class Task
 {
 public:
 	Task(Programmer * m_assignee);
+	bool IsDone() const;
 	void SetDone(bool isDone);
+	boost::signals2::connection DoOnDoneChange(
+		boost::signals2::signal<void(bool isDone)>::slot_type const & slot, bool doAction);
 
 private:
-	bool m_done = false;
+	bool m_isDone = false;
 	Programmer * m_assignee;
+	boost::signals2::signal<void(bool isDone)> m_doneChangeSignal;
 };
